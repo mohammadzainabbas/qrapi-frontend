8ea66623ff335d4fd3421d82c41870b2573dcab4
Mohammad Zain Abbas <mabbas@GlobalRescue.local>
2018-08-06 16:16:09 +0500

    Table dev

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,4 +21,40 @@ $(document).ready(function () {
 			}
 		});
 	});
+
+	$("#Load_Data").click(function (event){
+
+		$.ajax({
+			url:"localhost:8080/qrapi/api/v1/contacts/all",
+			type:"GET",
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success:function(contact_data)
+			{
+				var table_data = '<table class="table table-bordered table-striped">';
+				for  (var count =0; count < contact_data.length; count++)
+				{
+					table_data += "<tr>";
+					var cell_data = contact_data[count];
+					for (var cell_count = 0; cell_count < cell_data.length)
+					{
+						if(count === 0)
+						{
+							table_data += "<th>" + cell_data[cell_count] + "</th>";
+						}
+						else
+						{
+							table_data += "<td>" + cell_data[cell_count] + "</td>";
+						}
+					}
+					table_data += "</tr>";
+				}
+			}
+
+		})
+
+	});
+
+
+
 });
